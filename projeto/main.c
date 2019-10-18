7ff87e28b726c2a43efdc95f46d4229dd19a393b
PEDRO GOMES MOREIRA <u18174@academico.cotuca.unicamp.br>
2019-10-18 13:23:18 -0300

    quero fazer

--- projeto/main.c
+++ projeto/main.c
@@ -3,6 +3,20 @@
 #include "fila.h"
 #include "arvore.h"
 
+/*
+Algoritmo de Huffman:
+
+    1-  ler texto e guardar ocorrências de cada caractere
+    2-  ordenar ou priorizar caracteres pelo número de ocorrências
+    3-  construir árvore baseada na lista priorizada
+    4-  realizar percurso na árvore para determinar código de cada caractere
+    5-  ler texto de novo e botar o código no lugar de cada caractere
+        isso deve ser feito byte a byte, aglutinando os códigos
+
+    O arquivo final deve conter a quantidade de bits que são lixo (ao final)
+    e a lista priorizada para formar a árvore e realizar os percursos
+*/
+
 int main()
 {
     return 0;
