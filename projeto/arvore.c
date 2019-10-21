49ee25ae4859c49807d9f773d1959096b9d9e940
PEDRO GOMES MOREIRA <u18174@academico.cotuca.unicamp.br>
2019-10-21 08:04:28 -0300

    método novaArvore

--- projeto/arvore.c
+++ projeto/arvore.c
@@ -1,1 +1,13 @@
 #include "arvore.h"
+
+NoArvore *novaArvore(
+    InfoChar info,
+    NoArvore *esq,
+    NoArvore *dir)
+{
+    NoArvore *nova = (NoArvore*) malloc(sizeof(NoArvore));
+    nova->info = info;
+    nova->esquerda = esq;
+    nova->direita = dir;
+    return nova;
+}
