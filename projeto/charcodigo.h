5c6f93b7f80576c206e82af1391b26b54fd82e4a
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-10-23 10:47:42 -0300

    charcodigo

---
+++ projeto/charcodigo.h
@@ -0,0 +1,13 @@
+#ifndef CHARCODIGO
+#define CHARCODIGO
+
+#define MAX_STRING 17
+
+typedef
+    struct
+    {
+        char caractere;
+        char codigo[MAX_STRING];
+    } CharCodigo;
+
+#endif /*CHARCODIGO*/
