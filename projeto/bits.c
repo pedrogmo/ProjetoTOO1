ce0eb5777b05547316f4a53262860a55b9a01f78
GustavoHMeira <cc18015@g.unicamp.br>
2019-10-30 11:43:47 -0300

    isUm

--- projeto/bits.c
+++ projeto/bits.c
@@ -4,7 +4,16 @@ void setBit(
     unsigned char qualBit,
     char *byte)
 {
-    unsigned char bitDesejado = 0b10000000;
+    char bitDesejado = 0b10000000;
     bitDesejado >>= qualBit;
     *byte |= bitDesejado;
+}
+
+bool isUm(
+    unsigned char qualBit,
+    char byte)
+{
+    char byteComparacao = 0b10000000;
+    byteComparacao >>= qualBit;
+    return byteComparacao & byte;
 }
--- projeto/bits.h
+++ projeto/bits.h
@@ -1,8 +1,13 @@
 #ifndef BITS
 #define BITS
+#include "bool.h"
 
 extern void setBit(
     unsigned char qualBit,
     char *byte);
+
+extern bool isUm(
+    unsigned char qualBit,
+    char byte);
 
 #endif /*BITS*/
